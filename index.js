e7884b2adcb7d1f209bd7888c73bcd199f84b795
Artur <arturrsilveira85@gmail.com>
2024-09-10 20:56:03 -0300

    Listar metas finalizado

--- index.js
+++ index.js
@@ -26,19 +26,26 @@ const cadastrarMeta = async () => {
 const listarMetas = async () => {
   const respostas = await checkbox({
     message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
-    choices: [...metas]
+    choices: [...metas],
+    instructions: false,
   })
 
     if (respostas.length == 0 ) {
       console.log("Nenhuma Meta Selecionada")
       return
     }
+
+  metas.forEach((m)=>{
+    m.checked = false
+  })
+
   respostas.forEach((resposta)=> {
     const meta = metas.find((m)=>{
       return m.value == resposta
     })
     meta.checked = true;
-  })    
+  })
+  console.log("Meta(s) marcada(s) como concluída(s)! ")
 }
 
 const start = async () => {
